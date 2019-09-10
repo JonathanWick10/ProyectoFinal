0e36028f7e2fff35bdf6c479ee2fa0dd2fc77d5a
carolinavalencia93 <caritovalencia15@gmail.com>
2019-09-09 21:25:58 -0500

    create ListPatient

---
+++ app/src/main/java/com/jonathan/proyectofinal/ui/ListPatient.java
@@ -0,0 +1,16 @@
+package com.jonathan.proyectofinal.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jonathan.proyectofinal.R;
+
+public class ListPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_patient);
+    }
+}
