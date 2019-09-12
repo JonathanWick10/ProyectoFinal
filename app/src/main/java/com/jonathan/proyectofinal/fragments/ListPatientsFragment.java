69bf160a3d8787e0ac54beec166bd9a3210d7245
carolinavalencia93 <caritovalencia15@gmail.com>
2019-09-12 09:54:45 -0500

    add list patients fragment

--- app/src/main/java/com/jonathan/proyectofinal/fragments/ListPatientsFragment.java
+++ app/src/main/java/com/jonathan/proyectofinal/fragments/ListPatientsFragment.java
@@ -1,4 +1,31 @@
 package com.jonathan.proyectofinal.fragments;
 
-public class ListPatientsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jonathan.proyectofinal.R;
+
+import butterknife.BindArray;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ListPatientsFragment extends Fragment {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_patients, container, false);
+        ButterKnife.bind(this, view);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
