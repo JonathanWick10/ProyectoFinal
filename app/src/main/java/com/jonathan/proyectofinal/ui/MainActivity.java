c997474f13c64906323a7f127d6e04b5b62f965f
Jonathan Mosquera Alban <kzmojonathan@gmail.com>
2019-09-23 16:10:13 -0500

    i add file "CollectionCreation.java", new pakage "tools" with file "Constans.java", i modify the "MainActivity" and its xml, i add new resource "strings" in english. i modify el build gradle.

--- app/src/main/java/com/jonathan/proyectofinal/ui/MainActivity.java
+++ app/src/main/java/com/jonathan/proyectofinal/ui/MainActivity.java
@@ -3,14 +3,41 @@ package com.jonathan.proyectofinal.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.jonathan.proyectofinal.R;
+import com.jonathan.proyectofinal.database.CollectionCreation;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_create)
+    Button btnCreate;
+    @BindView(R.id.list_collections)
+    ListView listView;
+
+    CollectionCreation collectionCreation = new CollectionCreation();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,
+                        android.R.layout.simple_expandable_list_item_1,
+                        collectionCreation.createCollections(MainActivity.this));
+                listView.setAdapter(adapter);
+                btnCreate.setEnabled(true);
+            }
+        });
     }
 }
