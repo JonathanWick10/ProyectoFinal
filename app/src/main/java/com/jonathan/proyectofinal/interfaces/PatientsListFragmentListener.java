5d66957a8fc6bf2076ea1b5a18d7c558676dc757
carolinavalencia93 <caritovalencia15@gmail.com>
2019-09-23 21:10:53 -0500

    add interface PatientsListFragmentListener.java and modify PatientsList.java

---
+++ app/src/main/java/com/jonathan/proyectofinal/interfaces/PatientsListFragmentListener.java
@@ -0,0 +1,12 @@
+package com.jonathan.proyectofinal.interfaces;
+
+import com.jonathan.proyectofinal.data.Patient;
+
+import java.util.List;
+
+public interface PatientsListFragmentListener {
+
+
+        void addAllToList(List<Patient> patients);
+    }
+
--- app/src/main/java/com/jonathan/proyectofinal/ui/PatientsList.java
+++ app/src/main/java/com/jonathan/proyectofinal/ui/PatientsList.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.jonathan.proyectofinal.R;
+import com.jonathan.proyectofinal.fragments.hp.PatientsListFragment;
+import com.jonathan.proyectofinal.interfaces.PatientsListFragmentListener;
 
 public class PatientsList extends AppCompatActivity {
+
+    private PatientsListFragmentListener fragmentListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_patient);
+
+
+        PatientsListFragment fragment = (PatientsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentList);
+        fragment.setRetainInstance(true);
+        fragmentListener = (PatientsListFragmentListener) fragment;
     }
 }
