caca9d017a8c9927872b08c70dff4d7950b00fa7
qira03 <andreaqo@unicauca.edu.co>
2019-09-30 20:52:43 -0500

    Add files from health professional

---
+++ app/src/main/java/com/jonathan/proyectofinal/fragments/hp/GraphPSFragment.java
@@ -0,0 +1,25 @@
+package com.jonathan.proyectofinal.fragments.hp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.jonathan.proyectofinal.R;
+
+public class GraphPSFragment extends Fragment {
+
+    public GraphPSFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ps_graph, container, false);
+    }
+
+}
