bfbe2bb8c9153cd211084be761e1724d0401ef8b
Julian Sarzosa <ojsarzosa@misena.edu.co>
2019-10-01 20:56:56 -0500

    fix strings to English

--- app/src/main/java/com/jonathan/proyectofinal/fragments/carer/GeneralInformationFragment.java
+++ app/src/main/java/com/jonathan/proyectofinal/fragments/carer/GeneralInformationFragment.java
@@ -1,6 +1,7 @@
 package com.jonathan.proyectofinal.fragments.carer;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jonathan.proyectofinal.R;
+
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GeneralInformationFragment extends Fragment {
+
+    TextView textView;
 
 
     public GeneralInformationFragment() {
@@ -25,8 +31,12 @@ public class GeneralInformationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_general_information, container, false);
+        View view = inflater.inflate(R.layout.fragment_general_information, container, false);
+        textView = view.findViewById(R.id.textview);
+        //textView.setText(R.string.);
+
+
+        return view;
     }
 
 }
