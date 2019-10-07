082814430ed2203408f17ec7c90197ed3d02924f
Andrea <andreaqo@unicauca.edu.co>
2019-10-07 14:01:51 -0500

    Add Firebase Login with Google

--- app/src/main/java/com/jonathan/proyectofinal/database/LoginManager.java
+++ app/src/main/java/com/jonathan/proyectofinal/database/LoginManager.java
@@ -9,10 +9,15 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.jonathan.proyectofinal.R;
 import com.jonathan.proyectofinal.fragments.admin.AdminHome;
 import com.jonathan.proyectofinal.ui.Login;
 
@@ -21,8 +26,10 @@ import java.util.concurrent.Executor;
 public class LoginManager {
 
     private FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+    private FirebaseUser user = firebaseAuth.getCurrentUser();
+    private GoogleSignInClient googleSignInClient;
 
-    public void loginEmailPassword (final Context context, String email, String password){
+    public void emailPasswordLogin (final Context context, String email, String password){
 
 
         firebaseAuth.signInWithEmailAndPassword(email, password)
@@ -34,14 +41,52 @@ public class LoginManager {
                             context.startActivity(new Intent(context, AdminHome.class));
 
                         } else {
-                            // If sign in fails, display a message to the user.
-                            context.startActivity(new Intent(context, Login.class));
+                            Toast.makeText(context, context.getString(R.string.auth_fail),
+                                    Toast.LENGTH_SHORT).show();
 
                         }
-
-                        // ...
                     }
                 });
+    }
+
+    public void createUserWithEmailAndPassword(final Context context, String email, String password){
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(context, context.getString(R.string.success_regis),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "ERROR EN EL REGISTRO!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    public boolean userLoggedIn(){
+        Boolean loggedIn;
+        if (user != null) {
+            loggedIn = true;
+        }
+        else {
+            loggedIn = false;
+        }
+        return loggedIn;
+    }
+
+    public void loginwithGoogle(final Context context){
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(context.getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(context, gso);
     }
 
 }
--- app/src/main/java/com/jonathan/proyectofinal/ui/Login.java
+++ app/src/main/java/com/jonathan/proyectofinal/ui/Login.java
@@ -1,25 +1,42 @@
 package com.jonathan.proyectofinal.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.jonathan.proyectofinal.R;
 import com.jonathan.proyectofinal.database.LoginManager;
+import com.jonathan.proyectofinal.fragments.admin.AdminHome;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,17 +55,19 @@ public class Login extends AppCompatActivity {
     MaterialButton btnLoginEmailPass;
     @BindView(R.id.link_registrar)
     TextView register;
+    @BindView(R.id.btn_login_google)
+    SignInButton btnLoginGoogle;
 
-    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient googleSignInClient;
+    private int GOOGLE_SIGN_IN=1;
+
+    List<AuthUI.IdpConfig> providers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-       // reference();
-        firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = firebaseAuth.getCurrentUser();
 
         pass.addTextChangedListener(new TextWatcher() {
             @Override
@@ -83,74 +102,153 @@ public class Login extends AppCompatActivity {
 
             }
         });
+
     }
 
-    private void loginEmailPass() {
-        String email = name.getText().toString().trim();
-        String password = pass.getText().toString().trim();
+    private LoginManager loginInstance() {
+        LoginManager loginManager = new LoginManager();
+        return loginManager;
+    }
 
-        if (email.isEmpty()) {
-            txtname.setError("Nombre de usuario obligatorio");
-        }
-        if (password.isEmpty()) {
-            txtpass.setError("Contraseña obligatoria");
-        } else {
-            LoginManager loginManager = new LoginManager();
-            loginManager.loginEmailPassword(this, email, password);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (loginInstance().userLoggedIn() == true) {
+            startActivity(new Intent(Login.this, AdminHome.class));
         }
     }
 
-    /*********************************** REGISTRO PARA PROBAR *****************************************/
+    private void emailPassLogin() {
+
+        if (validateFields() != null) {
+            loginInstance().emailPasswordLogin(this, validateFields()[0], validateFields()[1]);
+        }
+
+    }
+
+
+
+    //////// login con google *****************************************************************************
+    private void googleLogin(){
+        //loginInstance().loginwithGoogle(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Toast.makeText(Login.this,"CORRECTO GOOGLE", Toast.LENGTH_SHORT).show();
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(Login.this,"ERRO GOOGLE", Toast.LENGTH_SHORT).show();
+                // ...
+                firebaseAuthWithGoogle(null);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        FirebaseAuth.getInstance().signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("GOOGLE", "signInWithCredential:success");
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    updateUI(user);
+                    Toast.makeText(Login.this,"OK", Toast.LENGTH_SHORT).show();
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("GOOGLE", "signInWithCredential:failure", task.getException());
+               //     Snackbar.make(findViewById(R.id.la), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                    updateUI(null);
+
+                    Toast.makeText(Login.this,"MAL", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser fUser) {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if(account != null){
+            String personName = account.getDisplayName();
+            String personGivenName = account.getGivenName();
+            String personFamilyName = account.getFamilyName();
+            String personEmail = account.getEmail();
+            String personId = account.getId();
+            Uri personPhoto = account.getPhotoUrl();
+
+            register.setText("Nombre:"+personName+ " GivenName"+personGivenName+" familiar:"+personFamilyName+" Email:"+personEmail+" ID:"+personId);
+        }
+    }
+
+    /***************************************************************************************/
+    /*********************************** REGISTRO LUEGO BORRAR ******************************/
     private void register() {
 
-        //pass.setError(Html.fromHtml("<font color='red'>hello</font>"));
+        if (validateFields() != null) {
+            loginInstance().createUserWithEmailAndPassword(this, validateFields()[0], validateFields()[1]);
+        }
+
+    }
+    /*************************************************************************************/
+    /*************************************************************************************/
+
+
+    private String[] validateFields() {
         String email = name.getText().toString().trim();
         String password = pass.getText().toString().trim();
+        String[] data = new String[2];
 
         if (email.isEmpty()) {
-            txtname.setError("Nombre de usuario obligatorio");
+            txtname.setError(getString(R.string.email_required));
+            data = null;
         }
         if (password.isEmpty()) {
-            txtpass.setError("Contraseña obligatoria");
+            txtpass.setError(getString(R.string.password_required));
+            data = null;
+        } else if (!email.isEmpty() && !password.isEmpty()) {
+            data[0] = email;
+            data[1] = password;
         } else {
-
-            firebaseAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Toast.makeText(Login.this, "Registro correcto",
-                                        Toast.LENGTH_SHORT).show();
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(Login.this, "ERROR EN EL REGISTRO!",
-                                        Toast.LENGTH_SHORT).show();
-
-                            }
-
-                            // ...
-                        }
-                    });
-
+            data = null;
         }
-
-
-        //    pass.requestFocus();
+        return data;
     }
 
     @OnClick({R.id.login_admin_btn_enter, R.id.link_registrar})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.login_admin_btn_enter:
-                loginEmailPass();
+                emailPassLogin();
                 break;
             case R.id.link_registrar:
                 register();
                 break;
+            case R.id.btn_login_google:
+                googleLogin();
+                break;
         }
     }
 
-    /*************************************************************************************/
-    /*************************************************************************************/
 }
