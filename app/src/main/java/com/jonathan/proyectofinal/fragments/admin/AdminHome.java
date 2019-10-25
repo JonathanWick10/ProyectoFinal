0c7ff1095e2d217768db4a4a9a498c93ddb6a248
Luigui665 <luisalbeiro6@hotmail.com>
2019-10-24 19:48:52 -0500

    Navigation view on admin module

--- app/src/main/java/com/jonathan/proyectofinal/fragments/admin/AdminHome.java
+++ app/src/main/java/com/jonathan/proyectofinal/fragments/admin/AdminHome.java
@@ -1,42 +1,68 @@
 package com.jonathan.proyectofinal.fragments.admin;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.jonathan.proyectofinal.R;
 import com.jonathan.proyectofinal.adapters.AdminListPSAdapter;
 import com.jonathan.proyectofinal.data.HealthcareProfessional;
 import com.jonathan.proyectofinal.fragments.games.Memorama;
+import com.jonathan.proyectofinal.ui.Login;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class AdminHome extends AppCompatActivity implements AdminAddHealthProfessional.OnFragmentInteractionListener {
+public class AdminHome extends AppCompatActivity implements AdminAddHealthProfessional.OnFragmentInteractionListener, NavigationView.OnNavigationItemSelectedListener {
 
     @BindView(R.id.babAdmin)
     BottomAppBar bottomAppBar;
     @BindView(R.id.fabAdmin)
     FloatingActionButton floatingActionButton;
+    @BindView(R.id.toolbarHp)
+    MaterialToolbar toolbar;
+
+    @BindView(R.id.drawer_layout_hp)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view_hp)
+    NavigationView navigationView;
     private boolean isFabTapped = false;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        firebaseAuth = FirebaseAuth.getInstance();
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
 
         if (savedInstanceState == null) {
             handleFrame(new AdminListPSFragment(alertDelete()));
@@ -73,7 +99,7 @@ public class AdminHome extends AppCompatActivity implements AdminAddHealthProfes
             floatingActionButton.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_go_back));
         } else {
             bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
-             handleFrame(new AdminListPSFragment(alertDelete()));
+            handleFrame(new AdminListPSFragment(alertDelete()));
             //handleFrame(new Memorama());
             floatingActionButton.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_person_add));
         }
@@ -88,6 +114,10 @@ public class AdminHome extends AppCompatActivity implements AdminAddHealthProfes
         fragmentTransaction.commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 
 
     @Override
@@ -96,10 +126,50 @@ public class AdminHome extends AppCompatActivity implements AdminAddHealthProfes
     }
 
     @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        String itemName = (String) item.getTitle();
+        closeDrawer();
+        switch (item.getItemId()){
+            case R.id.btn_logout:
+                firebaseAuth.signOut();
+                Intent intent = new Intent(AdminHome.this, Login.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
+    private void closeDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    private void openDrawer() {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    @Override
     public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            closeDrawer();
+        }
         super.onBackPressed();
         finish();
     }
+
+    /*
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                NavigationViewHP navigationDrawerFragment = new NavigationViewHP();
+                navigationDrawerFragment.show(getSupportFragmentManager(), navigationDrawerFragment.getTag());
+                break;
+            default:
+                return false;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    */
 
     /*private AdminListPSFragment.AdminListPSFragmentI addPsSalud(){
         AdminListPSFragment.AdminListPSFragmentI professionalI = new AdminListPSFragment.AdminListPSFragmentI() {
