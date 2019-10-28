4ba2ba0990fb1055750b2b081cab6215ce8f761c
carolinavalencia93 <caritovalencia15@gmail.com>
2019-10-28 00:28:44 -0500

    create POJO Memorizame.java, MemorizameFamilyFragmentGrid.java and MemorizameFamilyGridAdapter.java

---
+++ app/src/main/java/com/jonathan/proyectofinal/adapters/MemorizameFamilyGridAdapter.java
@@ -0,0 +1,7 @@
+package com.jonathan.proyectofinal.adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MemorizameFamilyGridAdapter  {
+    //extends RecyclerView.Adapter<MemorizameFamilyGridAdapter.Holder>
+}
