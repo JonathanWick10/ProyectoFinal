bc81db4e96b1cf41a1ca901bab4a33015732bfad
Luigui665 <luisalbeiro6@hotmail.com>
2019-10-30 11:50:30 -0500

    Adding toolbar to registration form

--- app/src/main/java/com/jonathan/proyectofinal/ui/Registration_Carer.java
+++ app/src/main/java/com/jonathan/proyectofinal/ui/Registration_Carer.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.jonathan.proyectofinal.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Registration_Carer extends AppCompatActivity {
+
+    @BindView(R.id.toolbar_registration_carer)
+    MaterialToolbar toolbar;
 
     @BindView(R.id.admin_createps_til_name)
     TextInputEditText firstNameHp;
@@ -21,5 +26,20 @@ public class Registration_Carer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration__carer);
 
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
