cf0273a76115f52e58a601d8d1b6792b34ac4563
carolinavalencia93 <caritovalencia15@gmail.com>
2019-11-15 21:18:18 -0500

    Starting work in StretchingExercise for carer

--- app/src/main/java/com/jonathan/proyectofinal/fragments/carer/ExerciseCarerFragment.java
+++ app/src/main/java/com/jonathan/proyectofinal/fragments/carer/ExerciseCarerFragment.java
@@ -3,18 +3,39 @@ package com.jonathan.proyectofinal.fragments.carer;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.jonathan.proyectofinal.R;
+import com.jonathan.proyectofinal.adapters.MemorizameFamilyGridAdapter;
+import com.jonathan.proyectofinal.data.Memorizame;
+import com.jonathan.proyectofinal.tools.Constants;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ExerciseCarerFragment extends Fragment {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
 
 
     public ExerciseCarerFragment() {
@@ -26,7 +47,45 @@ public class ExerciseCarerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise_carer, container, false);
+        View view= inflater.inflate(R.layout.fragment_exercise_carer, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
-}
+
+    private void initRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+     /*   String uid = user.getUid();
+        //FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        CollectionReference collectionReferenceMemorizame = db.collection(Constants.Memorizame);
+
+        collectionReferenceMemorizame.document(patient.getPatientUID()).collection(categoria).get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        memorizameList = new ArrayList<Memorizame>();
+                        for (QueryDocumentSnapshot documentSnapshopt :
+                                queryDocumentSnapshots) {
+                            memorizameM = documentSnapshopt.toObject(Memorizame.class);
+                            memorizameList.add(memorizameM);
+                        }
+                        adapter = new MemorizameFamilyGridAdapter(memorizameList,getActivity(),iSelectionMemorizame,iDeleteMemorizame);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setHasFixedSize(true);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Message", e.toString());
+                    }
+                });
+
+
+    }
+
+      */
+
+}}
