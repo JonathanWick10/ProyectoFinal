927821dcf2c376dbfc55257489dea041e75023b8
Julian Sarzosa <ojsarzosa@misena.edu.co>
2019-11-16 14:01:40 -0500

    backpressed

--- app/src/main/java/com/jonathan/proyectofinal/fragments/carer/ExerciseCarerFragment.java
+++ app/src/main/java/com/jonathan/proyectofinal/fragments/carer/ExerciseCarerFragment.java
@@ -21,10 +21,14 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.jonathan.proyectofinal.R;
 import com.jonathan.proyectofinal.adapters.MemorizameFamilyGridAdapter;
+import com.jonathan.proyectofinal.adapters.PhysicalExerciseAdapter;
 import com.jonathan.proyectofinal.data.Memorizame;
+import com.jonathan.proyectofinal.data.PhysicalExerciseEntity;
+import com.jonathan.proyectofinal.fragments.games.PhysicalExecise;
 import com.jonathan.proyectofinal.tools.Constants;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,7 +40,9 @@ public class ExerciseCarerFragment extends Fragment {
 
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
+    private View view;
 
+    private PhysicalExecise.PhysicalExeciseI physicalExeciseI;
 
     public ExerciseCarerFragment() {
         // Required empty public constructor
@@ -47,14 +53,18 @@ public class ExerciseCarerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_exercise_carer, container, false);
+        view= inflater.inflate(R.layout.fragment_exercise_carer, container, false);
         ButterKnife.bind(this, view);
+        initRecyclerView();
         return view;
     }
 
 
     private void initRecyclerView() {
+
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(new PhysicalExerciseAdapter(entities(),physicalExeciseI));
 
      /*   String uid = user.getUid();
         //FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -88,4 +98,90 @@ public class ExerciseCarerFragment extends Fragment {
 
       */
 
-}}
+}
+
+    private List<PhysicalExerciseEntity> entities(){
+
+        List<PhysicalExerciseEntity> lista = new ArrayList<>();
+        PhysicalExerciseEntity entity1 = new PhysicalExerciseEntity();
+        entity1.setNameExercise(getString(R.string.physical_nameExercise_resistence));
+        entity1.setDescripcion(getString(R.string.description_resistance_exercice));
+        entity1.setImage(R.drawable.exercise1);
+        entity1.setTime(300000);
+        lista.add(entity1);
+
+        // dos
+        PhysicalExerciseEntity entity2 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise2);
+        entity2.setTime(30000);
+        lista.add(entity2);
+
+        // dos
+        PhysicalExerciseEntity entity3 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise3);
+        entity2.setTime(30000);
+        lista.add(entity3);
+
+        // dos
+        PhysicalExerciseEntity entity4 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise4);
+        entity2.setTime(30000);
+        lista.add(entity4);
+
+        // dos
+        PhysicalExerciseEntity entity5 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise5);
+        entity2.setTime(30000);
+        lista.add(entity5);
+
+        // dos
+        PhysicalExerciseEntity entity6 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise6);
+        entity2.setTime(30000);
+        lista.add(entity6);
+
+        // dos
+        PhysicalExerciseEntity entity7 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise7);
+        entity2.setTime(30000);
+        lista.add(entity7);
+
+        // dos
+        PhysicalExerciseEntity entity8 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise8);
+        entity2.setTime(30000);
+        lista.add(entity8);
+
+        // dos
+        PhysicalExerciseEntity entity9 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise9);
+        entity2.setTime(30000);
+        lista.add(entity9);
+
+        // dos
+        PhysicalExerciseEntity entity10 = new PhysicalExerciseEntity();
+        entity2.setNameExercise(getString(R.string.physical_nameexercise_bodybuilding));
+        entity2.setDescripcion(getString(R.string.description_nameexercise_bodybuilding));
+        entity2.setImage(R.drawable.exercise10);
+        entity2.setTime(30000);
+        lista.add(entity10);
+
+        return  lista;
+    }
+}
